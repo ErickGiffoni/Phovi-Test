f9359cd945f07e9280289841f047ac732287d7ca
ErickGiffoni <giffoni.erick@gmail.com>
2020-12-29 11:12:52 -0300

    use Axios to download Trivia photoUrl and add it to Storage
    Obs.: problem with CORS persisting
    
    ~Finishes task 3

--- TestePhovi/src/pages/TriviaMaker.tsx
+++ TestePhovi/src/pages/TriviaMaker.tsx
@@ -2,14 +2,18 @@ import React, { useEffect, useRef, useState } from "react";
 import "../assets/styles/TriviaMaker.css";
 import "../assets/styles/global.css";
 
-import { firestore } from "../firebase";
+import { firestore, storage } from "../firebase";
 import triviaTypeOptions from "../utils/triviaTypeOptions";
+
+//import { downloadImage } from "../utils/downloadImage"
 
 import Header from "../components/Header/index";
 import ModalLoadTrivia from "../components/ModalLoadTrivia/index";
 import Form from "../components/Form/index";
 import GenerateTrivia from "../components/GenerateTrivia/index";
 import ButtonGroup from "../components/ButtonGroup/index";
+
+const axios = require("axios");
 
 interface TriviaQuestions {
   question: string;
@@ -196,8 +200,12 @@ function TriviaMaker() {
       .collection(triviaType)
       .doc(triviaTitle)
       .set(data)
+      .then( () => {
+        alert("Trivia Added !");
+      })
       .catch((err: any) => {
         console.error(err);
+        alert("Error while adding Trivia...\nCheck security rules");
       });
 
     await firestore
@@ -214,6 +222,33 @@ function TriviaMaker() {
       .catch((err: any) => {
         console.error(err);
       });
+
+    let photoUrlBuffer;
+
+    await axios({
+      method: "get",
+      url: data.photoUrl,
+      responseType: "arraybuffer",
+      headers: { "Access-Control-Allow-Origin": "http://localhost:3000" } //erro com o CORS nao permite o download...
+    })
+    .then( (response) => {
+      photoUrlBuffer = response.data;
+    })
+    .catch( (err) => {
+      console.log("Error while downloading image...");
+      console.error(err);
+      alert("Error while downloading image...");
+    })
+
+    await storage
+      .ref("games/trivias/photoUrl/" + data.title + ".jpg")
+      .put(photoUrlBuffer, {
+        contentType: 'image/jpg',
+      })
+      .catch( (error) => {
+        console.error(error);
+        alert("Storage: image was not added");
+      })
   };
 
   return (
